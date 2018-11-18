dd1a4da8fddd2dccba24d78fe1867be4fa5c3c80
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2018-11-18 14:05:15 +0200

    aa

--- .php
+++ .php
@@ -1,3 +1,7 @@
 <?php
-echo "Hello World!";
+echo shell_exec('(cd test && find . -iname \*.php -exec php -l {} \;)');
+
+$var = '..'|
+ech shell_exec('(cd test && find . -iname \*.php -exec echo {} \;)');
+
 ?>
